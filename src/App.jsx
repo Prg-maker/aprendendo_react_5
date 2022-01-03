156c05e72a9fcf54d972b99545a07be7f40a2833
Prg-Maker <df3296709@gmail.com>
2022-01-03 09:22:02 -0300

    fist commmit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import'./AppStyle.scss'
+
+
+
+function App() {
+  const message = "hello "
+
+  return (
+    <div className="container">
+      
+    </div>
+  )
+}
+
+export default App
